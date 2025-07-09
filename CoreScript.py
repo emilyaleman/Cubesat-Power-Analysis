b4976cc9e62f0dd3269911fbddd9b0982926bb57
emilyaleman <ealeman@mit.edu>
2025-07-09 10:37:22 +0200

    Update CoreScript.py

--- CoreScript.py
+++ CoreScript.py
@@ -245,12 +245,12 @@ class PowerAnalyzer:
         ])
         return np.identity(3) + skew + skew @ skew * ((1 - dot) / (np.linalg.norm(cross) ** 2))
 
-    def sun_vector_in_LVLH(self, t):
+    def sun_vector_in_LVLH_to_body(self, t):   #multiplied by R_rsn to get body 
         M = (t / self.orbit.period) * 2 * np.pi
-        return SunPosition.inertial_to_LVLH(self.sun_vector_inertial, self.inclination, self.RAAN, M)
+        return SunPosition.inertial_to_LVLH(self.sun_vector_inertial, self.inclination, self.RAAN, M) @ R_rsn
 
     def compute_power(self, t):
-        sun_vector_b = self.sun_vector_in_LVLH(t)
+        sun_vector_b = self.sun_vector_in_LVLH(t) 
         total_power = 0
         power_per_face = {}
         for face in self.sat.panel_faces:
